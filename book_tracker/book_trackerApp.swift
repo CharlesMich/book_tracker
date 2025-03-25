d6a4b35ae306bb4d9f0acec04ce26600ec14cf9d
CharlesMich <charleedongo@hotmail.com>
2025-03-24 19:49:06 -0500

    Initial Commit

---
+++ book_tracker/book_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  book_trackerApp.swift
+//  book_tracker
+//
+//  Created by Charles Michael on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct book_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
