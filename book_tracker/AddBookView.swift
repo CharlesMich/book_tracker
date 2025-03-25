fe637400c2ac994f5e5f1ff9280d42cf1cd1b826
CharlesMich <charleedongo@hotmail.com>
2025-03-24 23:53:59 -0500

    second commit

---
+++ book_tracker/AddBookView.swift
@@ -0,0 +1,61 @@
+//
+//  AddBookView.swift
+//  book_tracker
+//
+//  Created by Charles Michael on 3/24/25.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    
+    
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    @State private var title = ""
+    @State private var author = ""
+    @State private var genre = ""
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
+    @State private var rating = 3
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Title", text: $title)
+                    TextField("author", text: $author)
+                    
+                    Picker("Genre", selection: $genre){
+                        ForEach(genres, id: \.self){
+                            Text($0)
+                            
+                        }
+                    }
+                }
+                Section ("write a review") {
+                    TextEditor(text: $review)
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                }
+                Section {
+                    Button("Save"){
+                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        dismiss()
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+#Preview {
+    AddBookView()
+}
