5619fcb053a26dd5dc29c7f06bcb322d3eba2565
CharlesMich <charleedongo@hotmail.com>
2025-03-26 23:15:57 -0500

    abc

--- book_tracker/AddBookView.swift
+++ book_tracker/AddBookView.swift
@@ -38,15 +38,17 @@ struct AddBookView: View {
                 }
                 Section ("write a review") {
                     TextEditor(text: $review)
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6) {
-                            Text(String($0))
-                        }
-                    }
+//                    Picker("Rating", selection: $rating) {
+//                        ForEach(0..<6) {
+//                            Text(String($0))
+//                        }
+//                    }
+                    RatingView(rating: $rating)
                 }
                 Section {
                     Button("Save"){
                         let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(newBook)
                         dismiss()
                     }
                 }
--- book_tracker/ContentView.swift
+++ book_tracker/ContentView.swift
@@ -10,15 +10,43 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+//    @Query var books: [Book]
+//    @Query(sort: \Book.title) var books: [Book]
+//    @Query(sort: \Book.rating, order: .reverse) var books: [Book]
+//    @Query(sort: [SortDescriptor(\Book.title), SortDescriptor(\Book.author)]) var books: [Book]
+    @Query(sort: [SortDescriptor(\Book.title, order: .reverse)]) var books: [Book]
+    
     
     @State private var showingAddScreen = false
     
     var body: some View {
         NavigationStack {
-            Text("Count: \(books.count)")
+            List {
+                ForEach(books) { book in
+                    NavigationLink(value: book){
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading){
+                                Text(book.title)
+                                    .font(.headline)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    .navigationDestination(for: Book.self){ book in
+                        DetailView(book: book)
+                    }
+                }
+                .onDelete(perform: deleteBook)
+            }
                 .navigationTitle("BookHub")
                 .toolbar {
+                    ToolbarItem(placement: .topBarLeading){
+                        EditButton()
+                    }
                     ToolbarItem(placement: .topBarTrailing){
                         Button("Add book", systemImage: "plus"){
                             showingAddScreen.toggle()
@@ -28,6 +56,13 @@ struct ContentView: View {
                 .sheet(isPresented: $showingAddScreen){
                     AddBookView()
                 }
+        }
+    }
+    func deleteBook(at offsets: IndexSet){
+        for offset in offsets {
+            let book = books[offset]
+            
+            modelContext.delete(book)
         }
     }
 }
